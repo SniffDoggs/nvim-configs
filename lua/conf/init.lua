9f98e654ed33462556d721f9f32aded15026df8c
Adipto Ghosh <adipto.ghosh@gmail.com>
2026-05-18 07:48:24 +0000

    Fixed color schemes for typedef and structs and enums

--- lua/conf/init.lua
+++ lua/conf/init.lua
@@ -21,4 +21,25 @@ vim.g.clipboard = {
 }
 vim.opt.clipboard = "unnamedplus"
 
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    -- 1. Break the default links so they stop copying 'Type' or 'StorageClass' colors
+    vim.api.nvim_set_hl(0, "cTypedef", { link = "NONE" })
+    vim.api.nvim_set_hl(0, "cStructure", { link = "NONE" }) -- Controls 'struct'
+
+    -- 2. Apply your custom styling (e.g., Orange for typedef, Pink for struct/enum)
+    vim.api.nvim_set_hl(0, "cTypedef", { fg = "#cb5ced", italic = true })   
+    vim.api.nvim_set_hl(0, "cStructure", { fg = "#a668fc", italic = true }) 
+    
+    -- Note: 'enum' is technically controlled by cStructure in old C syntax, 
+    -- but you can explicitly define it just in case:
+    vim.api.nvim_set_hl(0, "cStorageClass", { fg = "#50FA7B" }) -- Bright Green (Optional: for 'static', 'register')
+  end,
+})
+
+-- Force an immediate reload of the colorscheme to trigger the autocmd right now
+vim.cmd("colorscheme " .. (vim.g.colors_name or "default"))
+
+
 
