b0cdc065efca003a8c3d71c1744782a40b765143
Adipto Ghosh <adipto.ghosh@gmail.com>
2026-05-28 13:26:38 +0000

    fix treesitter

--- lua/conf/plugins/treesitter.lua
+++ lua/conf/plugins/treesitter.lua
@@ -1,39 +1,18 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  tag = "v0.9.3",
   build = ":TSUpdate",
-
   config = function()
-    require("nvim-treesitter").setup({
-        ensure_installed = {
-          "c",
-          "cpp",
-          "python",
-          "html",
-          "css",
-          "matlab",
-          "javascript",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query"
-        },
-
-        sync_install = false,
-        auto_install = true,
-
-        autopairs = {
-          enable = true,
-        },
-        
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-
-        indent = {
-          enable = true,
-          --disable = { "python", "c" } -- these and some other langs don't work well
-        },
-      })
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "c", "cpp", "python", "html", "css",
+        "matlab", "javascript", "lua", "vim", "vimdoc", "query"
+      },
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
   end
 }
